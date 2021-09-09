3519a77b532741ec57aa7cc5c86cc8baed45fdc6
kee111 <eqrlgrey@gmail.com>
2021-09-09 13:46:17 +0900

    保存

--- src/App.js
+++ src/App.js
@@ -1,20 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import { nanoid } from "nanoid";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import Todo from "./components/Todo";
 
 function App(props) {
+    const [tasks, setTasks] = useState(props.tasks);
+
     function addTask(name) {
-        alert(name);
+        const newTask = {
+            id: "todo-" + nanoid(),
+            name: name,
+            completed: false,
+        };
+        setTasks([...tasks, newTask]);
     }
-    const taskList = props.tasks.map((task) => (
+
+    function toggleTaskCompleted(id) {
+        const updatedTasks = tasks.map((task) => {
+            // if this task has the same ID as the edited task
+            if (id === task.id) {
+                // use object spread to make a new object
+                // whose `completed` prop has been inverted
+                return { ...task, completed: !task.completed };
+            }
+            return task;
+        });
+        setTasks(updatedTasks);
+    }
+
+    const taskList = tasks.map((task) => (
         <Todo
             id={task.id}
             name={task.name}
             completed={task.completed}
             key={task.id}
+            toggleTaskCompleted={toggleTaskCompleted}
         />
     ));
+    const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+    const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
     return (
         <div className="todoapp stack-large">
             <Form addTask={addTask} />
@@ -23,7 +49,7 @@ function App(props) {
                 <FilterButton />
                 <FilterButton />
             </div>
-            <h2 id="list-heading">3 tasks remaining</h2>
+            <h2 id="list-heading">{headingText}</h2>
             <ul
                 role="list"
                 className="todo-list stack-large stack-exception"
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -8,6 +8,7 @@ export default function Todo(props) {
                     id={props.id}
                     type="checkbox"
                     defaultChecked={props.completed}
+                    onChange={() => props.toggleTaskCompleted(props.id)}
                 />
                 <label className="todo-label" htmlFor={props.id}>
                     {props.name}
